c510b1963c838d1ff780a4e25728ac7958b68b52
Bashamega <adambashaahmednaji@gmail.com>
2024-06-07 18:28:25 +0300

    Responsive design

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 export function Slideshow() {
     return (
-        <div className=" bg-[#0E204E] relative text-white w-3/4 flex">
-            <Image src={'/img/House.png'} width={300} height={0} alt="a house" className=" w-[50%]" />
-            <IoIosArrowBack size={30} className="absolute top-[50%] left-[2%] cursor-pointer"/>
-            <IoIosArrowForward size={30} className="absolute top-[50%] left-[42%] cursor-pointer"/>
-            <div className="font-manrope flex p-2 items-center">
-                <p>Pineapple Island is the most sought-after  community in Ibadan that values and puts the needs of their clients first. It is a territory with loft complexes that creates a country life and homely feeling that is integrated with industrial development. A perfect, professional built  abode that gives you the true taste of beauty and happiness.</p>
+        <div className=" bg-[#0E204E] relative text-white lg:w-3/4 w-full lg:flex">
+            <Image src={'/img/House.png'} width={300} height={0} alt="a house" className=" w-full" />
+            <IoIosArrowBack size={30} className="absolute top-[50%] left-[2%] cursor-pointer z-50"/>
+            <IoIosArrowForward size={30} className="absolute top-[50%] lg:left-[42%] right-[2%] z-50 cursor-pointer"/>
+            <div className="font-manrope flex p-2 items-center absolute lg:static top-0 left-0 w-full px-10 h-full bg-[rgba(0,0,0,.5)]">
+                <p className="text-center lg:text-right">Pineapple Island is the most sought-after  community in Ibadan that values and puts the needs of their clients first. It is a territory with loft complexes that creates a country life and homely feeling that is integrated with industrial development. A perfect, professional built  abode that gives you the true taste of beauty and happiness.</p>
             </div>
         </div>
     )
--- src/components/SlideshowSection.tsx
+++ src/components/SlideshowSection.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Slideshow } from "./Slideshow";
 export function SlideshowSection() {
     return (
-        <section className="flex relative mx-[188px] my-1 justify-between items-center">
+        <section className="lg:flex relative lg:mx-[188px] mx-2 my-1 justify-between items-center">
             <div className="font-playfair">
-                <h1 className="text-4xl">
+                <h1 className="text-4xl hidden lg:block">
                     Pineapple Island:
                 </h1>
-                <h1 className="text-4xl">
+                <h1 className="text-4xl hidden lg:block">
                     GenZ and Millenial
+                </h1>
+                <h1 className="text-4xl lg:hidden block">
+                    Pineapple Island: GenZ and Millenial
                 </h1>
                 <h1 className="text-4xl">
                     Abode
