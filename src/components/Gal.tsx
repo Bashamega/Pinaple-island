56d50529517e8f0bcfbb5189bb63dfb9c011f1eb
Bashamega <adambashaahmednaji@gmail.com>
2024-06-08 07:40:39 +0300

    Forgot this 🤣

--- src/components/Gal.tsx
+++ src/components/Gal.tsx
@@ -4,8 +4,11 @@ import React from "react";
 export function Gal() {
     return (
         <section className=" my-20   mt-20 px-[2%]">
-            <h1 className="text-2xl font-playfair text-center ">Galleries</h1>
-            <p className=" font-manrope text-center">a community that gives you a taste of happiness, a place you’ll love to live and an opportunity to build a home.</p>
+            <h1 className="text-2xl font-playfair text-center hidden lg:block ">Galleries</h1>
+            <p className=" font-manrope text-center hidden lg:block">a community that gives you a taste of happiness, a place you’ll love to live and an opportunity to build a home.</p>
+            <h1 className="text-2xl font-playfair text-center lg:hidden block ">Available Homes</h1>
+            <p className=" font-manrope text-center lg:hidden block mx-5">a community that gives you a taste of happiness, a place you’ll love to live and an opportunity to build a home.</p>
+        
             <div className="lg:grid grid-cols-3 gap-2 w-full hidden ">
                 {Array.from({ length: 9 }, (_, index) => (
                     <Image width={500} height={0} className="w-[500px]" src={`/img/gal/${index +1}.png`} alt={`Image ${index+1}`} />
