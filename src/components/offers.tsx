61816d164d3e07085745c01d519cf0d9a2520a68
Bashamega <adambashaahmednaji@gmail.com>
2024-06-08 06:11:13 +0300

    Text color Update

--- src/components/offers.tsx
+++ src/components/offers.tsx
@@ -12,15 +12,15 @@ export function Offers() {
                 <div className=" bg-[#0E204E] w-[100px] h-[180px] block" />
                 <div className=" ml-[20%] flex justify-evenly w-full">
                     <div className=" flex flex-col justify-center items-center">
-                        <FaTree size={50}/>
+                        <FaTree size={50} className="text-[#0E204E]"/>
                         <span>Eco Friendly</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaWifi size={50}/>
+                        <FaWifi size={50} className="text-[#0E204E]"/>
                         <span>Smart Homes</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaMoneyBill size={50}/>
+                        <FaMoneyBill size={50} className="text-[#0E204E]"/>
                         <span>Good  Rates</span>
                     </div>
                 </div>
@@ -30,47 +30,47 @@ export function Offers() {
                 <div className=" bg-[#0E204E] w-[100px] h-[180px] block" />
                 <div className=" ml-[20%] flex justify-evenly w-full">
                     <div className=" flex flex-col justify-center items-center">
-                        <FaRegLightbulb size={50}/>
+                        <FaRegLightbulb size={50} className="text-[#0E204E]"/>
                         <span>24/7 Electricitry</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaLock size={50}/>
+                        <FaLock size={50} className="text-[#0E204E]"/>
                         <span>Private Security</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <TbSettingsStar size={50}/>
+                        <TbSettingsStar size={50} className="text-[#0E204E]"/>
                         <span>Serviced Apartment</span>
                     </div>
                 </div>
             </div>
             <div className="lg:hidden block relative">
                 <Image src={'/img/Rectangle 7.png'} width={200} height={0} alt="Photo" className="w-full" />
-                <div className="bg-[#0E204E] text-white w-full absolute bottom-[1%] pb-10">
-                <div className=" flex justify-evenly w-full py-10">
+                <div className="bg-[#0E204E] text-white w-[90%] absolute bottom-[-10%] pb-10 left-0 mx-[5%]">
+                <div className=" flex justify-evenly w-full ">
                     <div className=" flex flex-col justify-center items-center">
-                        <FaTree size={50}/>
+                        <FaTree size={50} className="text-[#0E204E]"/>
                         <span>Eco Friendly</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaWifi size={50}/>
+                        <FaWifi size={50} className="text-[#0E204E]"/>
                         <span>Smart Homes</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaMoneyBill size={50}/>
+                        <FaMoneyBill size={50} className="text-[#0E204E]"/>
                         <span>Good  Rates</span>
                     </div>
                 </div>
                 <div className=" mt-2 flex justify-evenly w-full">
                     <div className=" flex flex-col justify-center items-center">
-                        <FaRegLightbulb size={50}/>
+                        <FaRegLightbulb size={50} className="text-[#0E204E]"/>
                         <span>24/7 Electricitry</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <FaLock size={50}/>
+                        <FaLock size={50} className="text-[#0E204E]"/>
                         <span>Private Security</span>
                     </div>
                     <div className=" flex flex-col justify-center items-center">
-                        <TbSettingsStar size={50}/>
+                        <TbSettingsStar size={50} className="text-[#0E204E]"/>
                         <span>Serviced Apartment</span>
                     </div>
                 </div>
